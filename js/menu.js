6ac3cdfa9497d3707d5c44b37ade879af311816f
jisantillan <javierignaciosantillan.u@gmail.com>
2022-01-31 04:05:13 -0300

    Se cargo el proyecto con sass

--- js/menu.js
+++
@@ -1,7 +0,0 @@
-const hamburguer = document.getElementById('hamburguer');
-const menuUl = document.getElementById('ul-menu');
-
-hamburguer.addEventListener('click' , () => {
-    menuUl.classList.toggle('show');
-});
-
